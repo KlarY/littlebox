62aa3a167ae2743c449717528cc434ae8dec3516
stamaimer <stamaimer@gmail.com>
2014-08-23 20:21:40 +0800

    test for content-length

--- littleboxsocket.cpp
+++ littleboxsocket.cpp
@@ -33,6 +33,8 @@ void LittleBoxSocket::parseRequest()
 
     QString body = components[1];
 
+    qDebug() << body;
+
     //============================================================================
 
     QStringList lines = headers.split(QRegExp("\r\n"));
